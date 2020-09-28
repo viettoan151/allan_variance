b2f1928dfabb3a9000073fe5c9048ec6929d0579
Dinir Imameev <dtimameev@briogroup.ru>
2020-09-28 10:56:41 +0300

    feat(script): add Rate Random Walk calculation * refactor axis_info calculation * add plot_n_save() function

--- scripts/allan.py
+++ scripts/allan.py
@@ -10,9 +10,47 @@ import os
 import matplotlib.pyplot as plt  # only for plotting, not required for calculations
 import math
 
-def getRandomWalkSegment(tau,sigma):
 
-    m = -0.5 # slope of random walk
+def getRRWSegment(tau, sigma, bias_idx):
+
+    m = 0.5  # slope of random walk
+    """""""""""""""""""""""""""""""""
+    " Find point where slope = -0.5 "
+    """""""""""""""""""""""""""""""""
+    # randomWalk = None
+    i = len(tau)-2  # start idx
+    stopTauPoint = math.log(tau[bias_idx])
+    idx = 1  # idx of intersection point btw tangent line and curve
+    mindiff = -1
+
+    while (math.log(tau[i]) > stopTauPoint):
+        leftLogTau = math.log(tau[i], 10)
+        leftLogSigma = math.log(sigma[i], 10)
+        rightLogTau = math.log(tau[i+1], 10)
+        rightLogSigma = math.log(sigma[i+1], 10)
+        slope = (rightLogSigma-leftLogSigma)/(rightLogTau-leftLogTau)
+
+        diff = abs(slope-m)
+        if (diff < mindiff or mindiff == -1):
+            mindiff = diff
+            idx = i
+        i = i - 1
+
+    """"""""""""""""""""""""""""""
+    " Project line to tau = 10^0 "
+    """"""""""""""""""""""""""""""
+    x2 = math.log(tau[idx], 10)
+    y2 = math.log(sigma[idx], 10)
+
+    x1 = math.log(3, 10)
+    y1 = y2-m*(x2-x1)
+
+    return (pow(10, x1), pow(10, y1), pow(10, x2), pow(10, y2))
+
+
+def getARWSegment(tau, sigma):
+
+    m = -0.5  # slope of random walk
     """""""""""""""""""""""""""""""""
     " Find point where slope = -0.5 "
     """""""""""""""""""""""""""""""""
@@ -21,14 +59,14 @@ def getRandomWalkSegment(tau,sigma):
     idx = 1
     mindiff = 999
     logTau = -999
-    while (logTau<0):
-        logTau = math.log(tau[i],10)
-        logSigma = math.log(sigma[i],10)
-        prevLogTau = math.log(tau[i-1],10)
-        prevLogSigma = math.log(sigma[i-1],10)
+    while (logTau < 0):
+        logTau = math.log(tau[i], 10)
+        logSigma = math.log(sigma[i], 10)
+        prevLogTau = math.log(tau[i-1], 10)
+        prevLogSigma = math.log(sigma[i-1], 10)
         slope = (logSigma-prevLogSigma)/(logTau-prevLogTau)
         diff = abs(slope-m)
-        if (diff<mindiff):
+        if (diff < mindiff):
             mindiff = diff
             idx = i
         i = i + 1
@@ -36,20 +74,115 @@ def getRandomWalkSegment(tau,sigma):
     """"""""""""""""""""""""""""""
     " Project line to tau = 10^0 "
     """"""""""""""""""""""""""""""
-    x1 = math.log(tau[idx],10)
-    y1 = math.log(sigma[idx],10)
+    x1 = math.log(tau[idx], 10)
+    y1 = math.log(sigma[idx], 10)
     x2 = 0
     y2 = m*(x2-x1)+y1
 
-    return (pow(10,x1),pow(10,y1),pow(10,x2),pow(10,y2))
+    return (pow(10, x1), pow(10, y1), pow(10, x2), pow(10, y2))
 
-def getBiasInstabilityPoint(tau,sigma):
+
+def get_bias_inst_idx(tau, sigma):
     i = 1
-    while (i<tau.size):
-        if (tau[i]>1) and ((sigma[i]-sigma[i-1])>0): # only check for tau > 10^0
+    while (i < tau.size):
+        if (tau[i] > 1) and ((sigma[i]-sigma[i-1]) > 0):  # only check for tau > 10^0
             break
         i = i + 1
-    return (tau[i],sigma[i])
+    return i
+
+# axis info - list of tuples (taus_used, adev, rate_rand_walk, ang_rand_walk)
+# res_type - string, either 'acceleration' or 'gyroscope'
+# resultsPath - path of directory where files will be saved
+def plot_n_save(axis_info, res_type, resultsPath):
+    # Plot Acceleration Result
+    plt.figure(figsize=(12, 8))
+    ax = plt.gca()
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+
+    av_rrt = 0
+    av_art = 0
+
+    fname = res_type + '_results'
+    f = open(resultsPath + fname + '.csv', 'wt')
+    try:
+        writer = csv.writer(f)
+
+        for x in range(0, 3):
+            taus_used = axis_info[x][0]
+            adev = axis_info[x][1]
+            rate_rand_walk = axis_info[x][2]
+            ang_rand_walk = axis_info[x][3]
+
+            # deviation value in point tau = 3, NoiseBias
+            av_rrt = av_rrt + rate_rand_walk[1]
+            # deviation value in point tau = 1, NoiseDensity
+            av_art = av_art + ang_rand_walk[3]
+            if x == 0:
+                writer.writerow(('Rate Random Walk X', rate_rand_walk[1]))
+                writer.writerow(('Angular Random Walk X', ang_rand_walk[1]))
+                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
+                         [ang_rand_walk[1], ang_rand_walk[3]],
+                         label='Angular random walk X',
+                         linestyle='dotted',
+                         linewidth=5)
+                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
+                         [rate_rand_walk[1], rate_rand_walk[3]],
+                         label='Rate random walk X',
+                         linestyle='dashed',
+                         linewidth=5)
+                plt.plot(taus_used, adev)
+            elif x == 1:
+                writer.writerow(('Rate Random Walk Y', rate_rand_walk[1]))
+                writer.writerow(('Angular Random Walk Y', ang_rand_walk[1]))
+                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
+                         [ang_rand_walk[1], ang_rand_walk[3]],
+                         label='Angular random walk Y',
+                         linestyle='dotted',
+                         linewidth=5)
+                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
+                         [rate_rand_walk[1], rate_rand_walk[3]],
+                         label='Rate random walk Y',
+                         linestyle='dashed',
+                         linewidth=5)
+                plt.plot(taus_used, adev)
+            elif x == 2:
+                writer.writerow(('Rate Random Walk Z', rate_rand_walk[1]))
+                writer.writerow(('Angular Random Walk Z', ang_rand_walk[1]))
+                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
+                         [ang_rand_walk[1], ang_rand_walk[3]],
+                         label='Angular random walk Z',
+                         linestyle='dotted',
+                         linewidth=5)
+                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
+                         [rate_rand_walk[1], rate_rand_walk[3]],
+                         label='Rate random walk Z',
+                         linestyle='dashed',
+                         linewidth=5)
+                plt.plot(taus_used, adev)
+
+        av_rrt = av_rrt / 3
+        av_art = av_art / 3
+        writer.writerow(('Rate Random Walk Avg', av_rrt))
+        writer.writerow(('Angular Random Walk Avg', av_art))
+    finally:
+        f.close()
+
+    title = 'Allan Deviation for '+res_type
+    fig_name = 'allan_'+res_type
+
+    plt.grid(True, which="both")
+    plt.title(title)
+    plt.xlabel('Tau (s)')
+    plt.ylabel('ADEV')
+    plt.legend()
+
+    for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                 ax.get_xticklabels() + ax.get_yticklabels()):
+        item.set_fontsize(20)
+    plt.show(block=False)
+    plt.savefig(resultsPath + fig_name)
+
 
 def main(args):
 
@@ -60,20 +193,19 @@ def main(args):
     """"""""""""""
     " Parameters "
     """"""""""""""
-    bagfile = rospy.get_param('~bagfile_path','~/data/static.bag')
-    topic = rospy.get_param('~imu_topic_name','/imu')
-    axis = rospy.get_param('~axis',0)
-    sampleRate = rospy.get_param('~sample_rate',100)
-    isDeltaType = rospy.get_param('~delta_measurement',False)
-    numTau = rospy.get_param('~number_of_lags',1000)
-    resultsPath = rospy.get_param('~results_directory_path',None)
+    bagfile = rospy.get_param('~bagfile_path', '~/data/static.bag')
+    topic = rospy.get_param('~imu_topic_name', '/imu')
+    sampleRate = rospy.get_param('~sample_rate', 100)
+    isDeltaType = rospy.get_param('~delta_measurement', False)
+    numTau = rospy.get_param('~number_of_lags', 1000)
+    resultsPath = rospy.get_param('~results_directory_path', None)
 
     """"""""""""""""""""""""""
     " Results Directory Path "
     """"""""""""""""""""""""""
     if resultsPath is None:
         paths = rospkg.get_ros_paths()
-        path = paths[1] # path to workspace's devel
+        path = paths[1]  # path to workspace's devel
         idx = path.find("ws/")
         workspacePath = path[0:(idx+3)]
         resultsPath = workspacePath + 'av_results/'
@@ -81,16 +213,17 @@ def main(args):
         if not os.path.isdir(resultsPath):
             os.mkdir(resultsPath)
 
-    print "\nResults will be save in the following directory: \n\n\t %s\n"%resultsPath
+    print "\nResults will be saved in the following directory: \n\n\t %s\n" % resultsPath
 
     """"""""""""""""""
     " Form Tau Array "
     """"""""""""""""""
     taus = [None]*numTau
 
-    cnt = 0;
-    for i in np.linspace(-2.0, 5.0, num=numTau): # lags will span from 10^-2 to 10^5, log spaced
-        taus[cnt] = pow(10,i)
+    cnt = 0
+    # lags will span from 10^-2 to 10^5, log spaced
+    for i in np.linspace(-2.0, 5.0, num=numTau):
+        taus[cnt] = pow(10, i)
         cnt = cnt + 1
 
     """""""""""""""""
@@ -98,9 +231,9 @@ def main(args):
     """""""""""""""""
     bag = rosbag.Bag(bagfile)
 
-    N = bag.get_message_count(topic) # number of measurement samples
+    N = bag.get_message_count(topic)  # number of measurement samples
 
-    data = np.zeros( (6,N) ) # preallocate vector of measurements
+    data = np.zeros((6, N))  # preallocate vector of measurements
 
     if isDeltaType:
         scale = sampleRate
@@ -109,134 +242,56 @@ def main(args):
 
     cnt = 0
     for topic, msg, t in bag.read_messages(topics=[topic]):
-        data[0,cnt] = msg.linear_acceleration.x * scale
-        data[1,cnt] = msg.linear_acceleration.y * scale
-        data[2,cnt] = msg.linear_acceleration.z * scale
-        data[3,cnt] = msg.angular_velocity.x * scale
-        data[4,cnt] = msg.angular_velocity.y * scale
-        data[5,cnt] = msg.angular_velocity.z * scale
+        data[0, cnt] = msg.linear_acceleration.x * scale
+        data[1, cnt] = msg.linear_acceleration.y * scale
+        data[2, cnt] = msg.linear_acceleration.z * scale
+        data[3, cnt] = msg.angular_velocity.x * scale
+        data[4, cnt] = msg.angular_velocity.y * scale
+        data[5, cnt] = msg.angular_velocity.z * scale
         cnt = cnt + 1
 
     bag.close()
 
-    print "[%0.2f seconds] Bagfile parsed\n"%(rospy.get_time()-t0)
+    print "[%0.2f seconds] Bagfile parsed\n" % (rospy.get_time()-t0)
 
-    """"""""""""""""""
-    " Allan Variance "
-    """"""""""""""""""
-    if axis is 0:
-        currentAxis = 1 # loop through all axes 1-6
-    else:
-        currentAxis = axis # just loop one time and break
+    accel_axis_info = []
+    gyro_axis_info = []
 
-    all_axis_info = []
+    # Acceleration
+    for axis in range(3):
+        (taus_used, adev, adev_err, adev_n) = allantools.oadev(
+            data[axis], data_type='freq', rate=float(sampleRate), taus=np.array(taus))
 
-    while (currentAxis <= 6):
-        (taus_used, adev, adev_err, adev_n) = allantools.oadev(data[currentAxis-1], data_type='freq', rate=float(sampleRate), taus=np.array(taus) )
+        bias_inst_idx = get_bias_inst_idx(
+            taus_used, adev)   # bias instability point idx
+        rate_rand_walk = getRRWSegment(taus_used, adev, bias_inst_idx)
+        ang_rand_walk = getARWSegment(taus_used, adev)
 
-        randomWalkSegment = getRandomWalkSegment(taus_used,adev)
-        biasInstabilityPoint = getBiasInstabilityPoint(taus_used,adev)
+        accel_axis_info.append(
+            (taus_used, adev, rate_rand_walk, ang_rand_walk))
 
-        randomWalk = randomWalkSegment[3]
-        biasInstability = biasInstabilityPoint[1]
-        
-        """""""""""""""
-        " Save as CSV "
-        """""""""""""""
-        if (currentAxis==1):
-            fname = 'allan_accel_x'
-            title = 'Allan Deviation: Accelerometer X'
-        elif (currentAxis==2):
-            fname = 'allan_accel_y'
-            title = 'Allan Deviation: Accelerometer Y'
-        elif (currentAxis==3):
-            fname = 'allan_accel_z'
-            title = 'Allan Deviation: Accelerometer Z'
-        elif (currentAxis==4):
-            fname = 'allan_gyro_x'
-            title = 'Allan Deviation: Gyroscope X'
-        elif (currentAxis==5):
-            fname = 'allan_gyro_y'
-            title = 'Allan Deviation: Gyroscope Y'
-        elif (currentAxis==6):
-            fname = 'allan_gyro_z'
-            title = 'Allan Deviation: Gyroscope Z'
+        print "[%0.2f seconds] Finished calculating allan variance for acceleration axis %d" % (rospy.get_time()-t0, axis)
 
-        print "[%0.2f seconds] Finished calculating allan variance - writing results to %s"%(rospy.get_time()-t0,fname)
+    # Gyro
+    for axis in range(3, 6):
+        (taus_used, adev, adev_err, adev_n) = allantools.oadev(
+            data[axis], data_type='freq', rate=float(sampleRate), taus=np.array(taus))
 
-        f = open(resultsPath + fname + '.csv', 'wt')
+        bias_inst_idx = get_bias_inst_idx(
+            taus_used, adev)   # bias instability point idx
+        rate_rand_walk = getRRWSegment(taus_used, adev, bias_inst_idx)
+        ang_rand_walk = getARWSegment(taus_used, adev)
 
-        try:
-            writer = csv.writer(f)
-            writer.writerow( ('Random Walk', 'Bias Instability') )
-            writer.writerow( (randomWalk, biasInstability) )
-            writer.writerow( ('Tau', 'AllanDev', 'AllanDevError', 'AllanDevN') )
-            for i in range(taus_used.size):
-                writer.writerow( (taus_used[i],adev[i],adev_err[i],adev_n[i])  )
-        finally:
-            f.close()
+        gyro_axis_info.append(
+            (taus_used, adev, rate_rand_walk, ang_rand_walk))
 
+        print "[%0.2f seconds] Finished calculating allan variance for gyro axis %d" % (rospy.get_time()-t0, axis)
 
-        all_axis_info.append((taus_used, adev, 
-                                randomWalkSegment, randomWalk,
-                                biasInstabilityPoint))
+    plot_n_save(accel_axis_info, 'acceleration', resultsPath)
+    plot_n_save(gyro_axis_info, 'gyroscope', resultsPath)
 
-        currentAxis = currentAxis + 1 + axis*6 # increment currentAxis also break if axis is not =0
+    inp = raw_input("Press Enter key to close figures and end program\n")
 
-
-    # Plot Acceleration Result 
-    plt.figure(figsize=(12,8))
-    ax = plt.gca()
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    for i in range(0, 3):
-        taus_used = all_axis_info[i][0]
-        adev = all_axis_info[i][1]
-        plt.plot(taus_used,adev)
-
-        plt.grid(True, which="both")
-        plt.title('Allan Deviation Accelerometer')
-        plt.xlabel('Tau (s)')
-        plt.ylabel('ADEV')
-
-        # plt.plot([randomWalkSegment[0],randomWalkSegment[2]],
-        #          [randomWalkSegment[1],randomWalkSegment[3]],'k--')
-        # plt.plot(1,randomWalk,'rx',markeredgewidth=2.5,markersize=14.0)
-        # plt.plot(biasInstabilityPoint[0],biasInstabilityPoint[1],'ro')
-
-        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-                    ax.get_xticklabels() + ax.get_yticklabels()):
-            item.set_fontsize(20)
-        plt.show(block=False)
-    plt.savefig(resultsPath + 'allan_accel')
-        
-    # Plot Gyro Result 
-    plt.figure(figsize=(12,8))
-    ax = plt.gca()
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    for i in range(3, 6):
-        taus_used = all_axis_info[i][0]
-        adev = all_axis_info[i][1]
-        plt.plot(taus_used,adev)
-
-        plt.grid(True, which="both")
-        plt.title('Allan Deviation Gyroscope')
-        plt.xlabel('Tau (s)')
-        plt.ylabel('ADEV')
-
-        # plt.plot([randomWalkSegment[0],randomWalkSegment[2]],
-        #          [randomWalkSegment[1],randomWalkSegment[3]],'k--')
-        # plt.plot(1,randomWalk,'rx',markeredgewidth=2.5,markersize=14.0)
-        # plt.plot(biasInstabilityPoint[0],biasInstabilityPoint[1],'ro')
-
-        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
-                    ax.get_xticklabels() + ax.get_yticklabels()):
-            item.set_fontsize(20)
-        plt.show(block=False)
-    plt.savefig(resultsPath + 'allan_gyro')
-        
-    inp=raw_input("Press Enter key to close figures and end program\n")
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
