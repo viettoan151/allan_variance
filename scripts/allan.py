44bd0e3a8d67accd820e30866be13bd408a90c2a
Dinir Imameev <dtimameev@briogroup.ru>
2020-10-05 15:57:30 +0300

    feat(random_walk): add calculation * change oadev to adev to speed up execution

--- scripts/allan.py
+++ scripts/allan.py
@@ -9,47 +9,33 @@ import rospkg
 import os
 import matplotlib.pyplot as plt  # only for plotting, not required for calculations
 import math
+import yaml
 
 
-def getRRWSegment(tau, sigma, bias_idx):
-
-    m = 0.5  # slope of random walk
+# https://www.mathworks.com/help/nav/ug/inertial-sensor-noise-analysis-using-allan-variance.html
+def random_walk(tau, adev):
+    # Find the index where the slope of the log-scaled Allan deviation is equal
+    # to the slope specified.
+    slope = 0.5  # slope of random walk
     """""""""""""""""""""""""""""""""
     " Find point where slope = -0.5 "
     """""""""""""""""""""""""""""""""
-    # randomWalk = None
-    i = len(tau)-2  # start idx
-    stopTauPoint = math.log(tau[bias_idx])
-    idx = 1  # idx of intersection point btw tangent line and curve
-    mindiff = -1
+    logtau = np.array(list(map(lambda i: math.log10(i), tau)), dtype=np.float64)
+    logadev = np.array(list(map(lambda i: math.log10(i), adev)), dtype=np.float64)
+    dlogadev = np.diff(logadev) / np.diff(logtau)
+    i = np.argmin(dlogadev)   
 
-    while (math.log(tau[i]) > stopTauPoint):
-        leftLogTau = math.log(tau[i], 10)
-        leftLogSigma = math.log(sigma[i], 10)
-        rightLogTau = math.log(tau[i+1], 10)
-        rightLogSigma = math.log(sigma[i+1], 10)
-        slope = (rightLogSigma-leftLogSigma)/(rightLogTau-leftLogTau)
+    #  Find the y-intercept of the line.
+    log_b = logadev[i] - slope*logtau[i]
+    # Determine the rate random walk coefficient from the line.
+    log_k = slope*math.log10(3) + log_b
 
-        diff = abs(slope-m)
-        if (diff < mindiff or mindiff == -1):
-            mindiff = diff
-            idx = i
-        i = i - 1
+    K = pow(10, log_k)
 
-    """"""""""""""""""""""""""""""
-    " Project line to tau = 10^0 "
-    """"""""""""""""""""""""""""""
-    x2 = math.log(tau[idx], 10)
-    y2 = math.log(sigma[idx], 10)
-
-    x1 = math.log(3, 10)
-    y1 = y2-m*(x2-x1)
-
-    return (pow(10, x1), pow(10, y1), pow(10, x2), pow(10, y2))
+    return K
 
 
-def getARWSegment(tau, sigma):
-
+def white_noise(tau, sigma):
     m = -0.5  # slope of random walk
     """""""""""""""""""""""""""""""""
     " Find point where slope = -0.5 "
@@ -82,106 +68,60 @@ def getARWSegment(tau, sigma):
     return (pow(10, x1), pow(10, y1), pow(10, x2), pow(10, y2))
 
 
-def get_bias_inst_idx(tau, sigma):
-    i = 1
-    while (i < tau.size):
-        if (tau[i] > 1) and ((sigma[i]-sigma[i-1]) > 0):  # only check for tau > 10^0
-            break
-        i = i + 1
-    return i
-
 # axis info - list of tuples (taus_used, adev, rate_rand_walk, ang_rand_walk)
 # res_type - string, either 'acceleration' or 'gyroscope'
 # resultsPath - path of directory where files will be saved
-def plot_n_save(axis_info, res_type, resultsPath):
+def plot_n_save(axis_info, fig_name, results_path):
     # Plot Acceleration Result
     plt.figure(figsize=(12, 8))
     ax = plt.gca()
     ax.set_yscale('log')
     ax.set_xscale('log')
 
-    av_rrt = 0
-    av_art = 0
+    avg_w = 0
+    avg_b = 0
 
-    fname = res_type + '_results'
-    f = open(resultsPath + fname + '.csv', 'wt')
-    try:
-        writer = csv.writer(f)
+    for i in range(3):
+        taus_used = axis_info[i][0]
+        adev = axis_info[i][1]
 
-        for x in range(0, 3):
-            taus_used = axis_info[x][0]
-            adev = axis_info[x][1]
-            rate_rand_walk = axis_info[x][2]
-            ang_rand_walk = axis_info[x][3]
+        K = axis_info[i][2]
+        white_noise = axis_info[i][3]
+        
+        avg_w = avg_w + white_noise[3]
+        avg_b = avg_b + K
+        
+        lineK = np.array(list(map(lambda t: math.sqrt(t/3) * K, taus_used)), dtype=np.float64)
 
-            # deviation value in point tau = 3, NoiseBias
-            av_rrt = av_rrt + rate_rand_walk[1]
-            # deviation value in point tau = 1, NoiseDensity
-            av_art = av_art + ang_rand_walk[3]
-            if x == 0:
-                writer.writerow(('Rate Random Walk X', rate_rand_walk[1]))
-                writer.writerow(('Angular Random Walk X', ang_rand_walk[1]))
-                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
-                         [ang_rand_walk[1], ang_rand_walk[3]],
-                         label='Angular random walk X',
-                         linestyle='dotted',
-                         linewidth=5)
-                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
-                         [rate_rand_walk[1], rate_rand_walk[3]],
-                         label='Rate random walk X',
-                         linestyle='dashed',
-                         linewidth=5)
-                plt.plot(taus_used, adev)
-            elif x == 1:
-                writer.writerow(('Rate Random Walk Y', rate_rand_walk[1]))
-                writer.writerow(('Angular Random Walk Y', ang_rand_walk[1]))
-                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
-                         [ang_rand_walk[1], ang_rand_walk[3]],
-                         label='Angular random walk Y',
-                         linestyle='dotted',
-                         linewidth=5)
-                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
-                         [rate_rand_walk[1], rate_rand_walk[3]],
-                         label='Rate random walk Y',
-                         linestyle='dashed',
-                         linewidth=5)
-                plt.plot(taus_used, adev)
-            elif x == 2:
-                writer.writerow(('Rate Random Walk Z', rate_rand_walk[1]))
-                writer.writerow(('Angular Random Walk Z', ang_rand_walk[1]))
-                plt.plot([ang_rand_walk[0], ang_rand_walk[2]],
-                         [ang_rand_walk[1], ang_rand_walk[3]],
-                         label='Angular random walk Z',
-                         linestyle='dotted',
-                         linewidth=5)
-                plt.plot([rate_rand_walk[0], rate_rand_walk[2]],
-                         [rate_rand_walk[1], rate_rand_walk[3]],
-                         label='Rate random walk Z',
-                         linestyle='dashed',
-                         linewidth=5)
-                plt.plot(taus_used, adev)
-
-        av_rrt = av_rrt / 3
-        av_art = av_art / 3
-        writer.writerow(('Rate Random Walk Avg', av_rrt))
-        writer.writerow(('Angular Random Walk Avg', av_art))
-    finally:
-        f.close()
-
-    title = 'Allan Deviation for '+res_type
-    fig_name = 'allan_'+res_type
-
+        # plot Allan Deviation
+        plt.plot(taus_used, adev)
+        # plot bias function
+        plt.plot(taus_used, lineK, color='b', linewidth=2, alpha=0.5)  
+        plt.plot(3, K, 'o', color='r')
+        # plot white noise
+        plt.plot([white_noise[0], white_noise[2]],
+                 [white_noise[1], white_noise[3]], 
+                 'r', linewidth=2, alpha=0.5)
+    
+    avg_w = avg_w / 3
+    avg_b = avg_b / 3
+    
+    with open(os.path.join(results_path, fig_name+'.yaml'), 'w') as f:
+        coeffs = {'White Noise': float(avg_w), 'Random Walk': float(avg_b)}
+        yaml.dump(coeffs, f)
+    
+    title = 'Allan deviation ('+fig_name+')'
     plt.grid(True, which="both")
     plt.title(title)
     plt.xlabel('Tau (s)')
     plt.ylabel('ADEV')
-    plt.legend()
 
     for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
                  ax.get_xticklabels() + ax.get_yticklabels()):
         item.set_fontsize(20)
+
     plt.show(block=False)
-    plt.savefig(resultsPath + fig_name)
+    plt.savefig(os.path.join(results_path, fig_name))
 
 
 def main(args):
@@ -195,36 +135,30 @@ def main(args):
     """"""""""""""
     bagfile = rospy.get_param('~bagfile_path', '~/data/static.bag')
     topic = rospy.get_param('~imu_topic_name', '/imu')
-    sampleRate = rospy.get_param('~sample_rate', 100)
-    isDeltaType = rospy.get_param('~delta_measurement', False)
-    numTau = rospy.get_param('~number_of_lags', 1000)
-    resultsPath = rospy.get_param('~results_directory_path', None)
+    sample_rate = rospy.get_param('~sample_rate', 100)
+    is_delta_type = rospy.get_param('~delta_measurement', False)
+    num_tau = rospy.get_param('~number_of_lags', 1000)
+    results_path = rospy.get_param('~results_directory_path', None)
 
     """"""""""""""""""""""""""
     " Results Directory Path "
     """"""""""""""""""""""""""
-    if resultsPath is None:
+    if results_path is None:
         paths = rospkg.get_ros_paths()
         path = paths[1]  # path to workspace's devel
         idx = path.find("ws/")
-        workspacePath = path[0:(idx+3)]
-        resultsPath = workspacePath + 'av_results/'
+        workspace_path = path[0:(idx+3)]
+        results_path = workspace_path + 'av_results/'
 
-        if not os.path.isdir(resultsPath):
-            os.mkdir(resultsPath)
+        if not os.path.isdir(results_path):
+            os.mkdir(results_path)
 
-    print "\nResults will be saved in the following directory: \n\n\t %s\n" % resultsPath
+    print "\nResults will be saved in the following directory: \n\n\t %s\n" % results_path
 
     """"""""""""""""""
     " Form Tau Array "
     """"""""""""""""""
-    taus = [None]*numTau
-
-    cnt = 0
-    # lags will span from 10^-2 to 10^5, log spaced
-    for i in np.linspace(-2.0, 5.0, num=numTau):
-        taus[cnt] = pow(10, i)
-        cnt = cnt + 1
+    taus = np.logspace(-2.0, 5.0, num=num_tau)
 
     """""""""""""""""
     " Parse Bagfile "
@@ -235,7 +169,7 @@ def main(args):
 
     data = np.zeros((6, N))  # preallocate vector of measurements
 
-    if isDeltaType:
+    if is_delta_type:
         scale = sampleRate
     else:
         scale = 1.0
@@ -259,36 +193,32 @@ def main(args):
 
     # Acceleration
     for axis in range(3):
-        (taus_used, adev, adev_err, adev_n) = allantools.oadev(
-            data[axis], data_type='freq', rate=float(sampleRate), taus=np.array(taus))
+        (taus_used, adev, adev_err, adev_n) = allantools.adev(
+            data[axis], data_type='freq', rate=float(sample_rate), taus=taus)
 
-        bias_inst_idx = get_bias_inst_idx(
-            taus_used, adev)   # bias instability point idx
-        rate_rand_walk = getRRWSegment(taus_used, adev, bias_inst_idx)
-        ang_rand_walk = getARWSegment(taus_used, adev)
+        rand_walk = random_walk(taus_used, adev)
+        w_noise = white_noise(taus_used, adev)
 
         accel_axis_info.append(
-            (taus_used, adev, rate_rand_walk, ang_rand_walk))
+            (taus_used, adev, rand_walk, w_noise))
 
         print "[%0.2f seconds] Finished calculating allan variance for acceleration axis %d" % (rospy.get_time()-t0, axis)
 
     # Gyro
     for axis in range(3, 6):
-        (taus_used, adev, adev_err, adev_n) = allantools.oadev(
-            data[axis], data_type='freq', rate=float(sampleRate), taus=np.array(taus))
+        (taus_used, adev, adev_err, adev_n) = allantools.adev(
+            data[axis], data_type='freq', rate=float(sample_rate), taus=np.array(taus))
 
-        bias_inst_idx = get_bias_inst_idx(
-            taus_used, adev)   # bias instability point idx
-        rate_rand_walk = getRRWSegment(taus_used, adev, bias_inst_idx)
-        ang_rand_walk = getARWSegment(taus_used, adev)
+        rand_walk = random_walk(taus_used, adev)
+        w_noise = white_noise(taus_used, adev)
 
         gyro_axis_info.append(
-            (taus_used, adev, rate_rand_walk, ang_rand_walk))
+            (taus_used, adev, rand_walk, w_noise))
 
         print "[%0.2f seconds] Finished calculating allan variance for gyro axis %d" % (rospy.get_time()-t0, axis-3)
 
-    plot_n_save(accel_axis_info, 'acceleration', resultsPath)
-    plot_n_save(gyro_axis_info, 'gyroscope', resultsPath)
+    plot_n_save(accel_axis_info, 'acceleration', results_path)
+    plot_n_save(gyro_axis_info, 'gyroscope', results_path)
 
     inp = raw_input("Press Enter key to close figures and end program\n")
 
